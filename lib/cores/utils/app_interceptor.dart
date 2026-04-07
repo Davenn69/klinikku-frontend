25ead965a50dadf8e554125cbff4e348f8453f48
Davenn69 <devendeven98@gmail.com>
2026-04-07 13:20:48 +0700

    added : app interceptor

---
+++ lib/cores/utils/app_interceptor.dart
@@ -0,0 +1,23 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+
+class AppInterceptor extends Interceptor {
+  final Dio dio;
+
+  AppInterceptor(this.dio);
+
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) {
+    if (kDebugMode) {
+      print('==== ERROR INTERCEPTOR START ====');
+      print('$err');
+      print('uri ${err.requestOptions.uri}');
+      print('path ${err.requestOptions.path}');
+      print('params ${err.requestOptions.queryParameters}');
+      print('message ${err.message}');
+      print('response ${err.response}');
+      print('error status code ${err.response?.statusCode}');
+      print('==== ERROR INTERCEPTOR END ====');
+    }
+  }
+}
