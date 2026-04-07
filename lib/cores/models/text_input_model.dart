0eb4d225a57c27cf6a29e65797709ae6b267772f
Davenn69 <devendeven98@gmail.com>
2026-04-07 15:12:06 +0700

    added : custom text field component

---
+++ lib/cores/models/text_input_model.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TextInputModel {
+  final controller = TextEditingController();
+  String? Function(String)? validator;
+
+  // Getter
+  String get text => controller.text.trim();
+  String get untrimmedText => controller.text;
+  String get textWithNoComma => controller.text.replaceAll(',', '');
+
+  // Setter
+  set text(String value) => controller.text = value;
+
+  // Clear form
+  void clear() => controller.clear();
+
+  TextInputModel({this.validator});
+}
