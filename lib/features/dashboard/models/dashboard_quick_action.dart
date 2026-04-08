909429a6bbdf3dab8f2c9713989230e6dbc9dcb3
Davenn69 <devendeven98@gmail.com>
2026-04-08 14:37:07 +0700

    added : dashboard widgets and models

---
+++ lib/features/dashboard/models/dashboard_quick_action.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DashboardQuickAction {
+  final String title;
+  final IconData icon;
+  final Color iconBackgroundColor;
+  final Color iconColor;
+
+  const DashboardQuickAction({
+    required this.title,
+    required this.icon,
+    required this.iconBackgroundColor,
+    required this.iconColor,
+  });
+}
---
+++ lib/features/dashboard/models/upcoming_booking.dart
@@ -0,0 +1,15 @@
+class UpcomingBooking {
+  final String scheduleLabel;
+  final String doctorName;
+  final String specialization;
+  final String status;
+  final String bookingCode;
+
+  const UpcomingBooking({
+    required this.scheduleLabel,
+    required this.doctorName,
+    required this.specialization,
+    required this.status,
+    required this.bookingCode,
+  });
+}
