31e1856c9eae575b9cb462646e9e54147744a165
Davenn69 <devendeven98@gmail.com>
2026-04-09 09:17:49 +0700

    added : booking detail api integration

--- lib/features/booking/viewmodels/booking_detail_viewmodel.dart
+++ lib/features/booking/viewmodels/booking_detail_viewmodel.dart
@@ -1,14 +1,38 @@
 import 'dart:async';
 
+import 'package:dio/dio.dart';
 import 'package:klinikku/cores/bases/base_notifier.dart';
+import 'package:klinikku/cores/mixins/toast_mixin.dart';
 import 'package:klinikku/features/booking/models/booking_model.dart';
+import 'package:klinikku/features/booking/services/booking_detail_services.dart';
 
-class BookingDetailVM extends BaseNotifier {
+class BookingDetailVM extends BaseNotifier with ToastMixin {
   final String bookingId;
   late BookingModel detail;
+
+  final BookingDetailServices _service = BookingDetailServices();
 
   BookingDetailVM(super.ref, {required this.bookingId});
 
   @override
-  FutureOr<void> init() {}
+  FutureOr<void> init() async {
+    await getBookingDetail();
+  }
+
+  getBookingDetail() async {
+    final response = await _service.getDetail(bookingId);
+
+    if (response is DioException) {
+      String errorMsg =
+          response.response == null
+              ? "Internal server error."
+              : response.response!.data['error']['message'];
+      showErrorToast(errorMsg);
+      return;
+    }
+
+    final data = response.data['encounter'];
+
+    detail = BookingModel.fromResponseBody(data);
+  }
 }
